02a289756572531ea7d5694b04ca4a511864be8e
unknown <k90218104@gcloud.csu.edu.tw>
2017-07-31 22:14:10 +0800

    更改連線設定 in 20170731

--- connect/connect.php
+++ connect/connect.php
@@ -1,9 +1,9 @@
 <?php
   try {
-    $username = 'u235232766_roo';
+    $username = 'root';
     $password = '12345678';
-    $dbname="u235232766_book";
-    $dsn = "mysql:host=mysql.hostinger.com.hk;dbname=$dbname";
+    $dbname="recycle";
+    $dsn = "mysql:host=localhost;dbname=$dbname";
            $options = array(
                    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
                  );
